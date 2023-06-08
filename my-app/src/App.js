6fe66132e3fcb953f06687109d23fd3adf1f4f5d
Andy <Andy_lam20@outlook.com>
2023-06-08 00:00:53 -0400

    Made pokdex

---
+++ my-app/src/App.js
@@ -0,0 +1,51 @@
+import {useState, useEffect, useRef} from "react"
+import PokemonThumbnail from "./components/PokemonThumbnail.js"
+
+function App() {
+    const [allPokemon, setAllPokemon] = useState([])
+    const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=20')
+
+    const getAllPokemons = async () => {
+      const res = await fetch(loadMore)
+      const data = await res.json()
+  
+      setLoadMore(data.next)
+
+      data.results.map(async (pokemon) => {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+        const data = await res.json()
+        setAllPokemon(currentList => [...currentList, data])
+      })
+    }
+
+    const shouldGetAllPokemon = useRef(true)
+
+    useEffect(() => {
+      if(shouldGetAllPokemon.current) {
+        shouldGetAllPokemon.current = false
+        getAllPokemons()
+      }
+    })
+
+    return (
+      <div className="app-container">
+        <h1>Test</h1>
+        <div className="pokemon-container">
+          <div className="all-container">
+          {allPokemon.map((pokemon, index) => (
+              <PokemonThumbnail
+                key={index}
+                id={pokemon.id}
+                name={pokemon.name}
+                image={pokemon.sprites.other.dream_world.front_default}
+                type={pokemon.types[0].type.name}
+              />
+          ))}
+          </div>
+        </div>
+        <button className="load-more" onClick={() => getAllPokemons()}>Load More</button>
+      </div>
+    );
+}
+
+export default App;
