9ba72f08997cfd12cfd241e9b1e85ddd4ba4ecc6
Andy <Andy_lam20@outlook.com>
2023-06-29 17:32:25 -0400

    Added to Pokemon

--- my-app/src/pages/Pokemon.jsx
+++ my-app/src/pages/Pokemon.jsx
@@ -1,10 +1,42 @@
 import { useParams} from "react-router-dom"
+import { useEffect, useState, useRef } from "react"
 
 export default function Pokemon() {
+    const [pokemonData, setPokemonData] = useState(null)
     const params = useParams()
-    console.log(params)
+    
+    const getPokemon = async (pokemon) => {
+        console.log(pokemon.pokemon)
+        try {
+            const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.pokemon}`)
+            const data = await res.json()
+            console.log(data)
+            setPokemonData(data)
+        } catch(error) {
+            console.log(error)
+        }
+
+    }
+
+    const shouldGetAllPokemon = useRef(true)
+
+    useEffect(() => {
+      if(shouldGetAllPokemon.current) {
+        shouldGetAllPokemon.current = false
+        getPokemon(params)
+      }
+    }, [params])
+
+    console.log(pokemonData)
 
     return (
-        <h1>{params.pokemon}</h1>
+        <>
+            {pokemonData ?
+            <>
+            <h1>{pokemonData.name}</h1>
+            <h2>Test</h2>
+            </>: 
+            <h1>Loading . . .</h1>}
+        </>
     )
 }
